2bf4a3144490e695ae8bfe189a23810f845c461e
saifaldin14 <saifaldindeveloper@gmail.com>
2020-12-26 09:23:40 -0500

    Create global.dart
    Created global styles to be used throughout the app

---
+++ App/lib/models/global.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0xFF212128);
+Color lightBlueColor = new Color(0xFF8787A0);
+Color redColor = new Color(0xFFDC4F64);
+
+TextStyle intrayTitleStyle = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 50);
+
+TextStyle bigLightBlueTitle = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: lightBlueColor,
+    fontSize: 50);
+
+TextStyle darkTodoTitle = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 30);
+
+TextStyle whiteTitle = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    fontSize: 20);
+
+TextStyle whiteButtonTitle = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    fontSize: 15);
+TextStyle redTodoTitle = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: redColor,
+    fontSize: 30);
+
+TextStyle redBoldText = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: redColor,
+    fontSize: 20);
+
+TextStyle redText =
+    new TextStyle(fontFamily: 'Avenir', color: redColor, fontSize: 20);
