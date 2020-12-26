d8cd98e816ceb042c72c709edcecaaf1ec3385a2
saifaldin14 <saifaldindeveloper@gmail.com>
2020-12-26 09:25:54 -0500

    Create task.dart
    Created tasks within the app and what's the state and propeties of the task

---
+++ App/lib/models/classes/task.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+class Task {
+  List<Task> tasks;
+  String note;
+  DateTime timeToComplete;
+  bool completed;
+  String repeats;
+  DateTime deadline;
+  List<DateTime> reminders;
+  int taskId;
+  String title;
+
+  Task(this.title, this.completed, this.taskId, this.note);
+
+  factory Task.fromJson(Map<String, dynamic> parsedJson) {
+    return Task(
+      parsedJson['title'],
+      parsedJson['completed'],
+      parsedJson['id'],
+      parsedJson['note'],
+    );
+  }
+}
