a4e9fe3f2812fbab31825f620d9f3950e1b9cace
saifaldin14 <saifaldindeveloper@gmail.com>
2020-12-27 14:16:41 -0500

    Update backend and added necessary packages

--- Backend/app.py
+++ Backend/app.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 from resources.Register import Register
 from resources.Signin import Signin
-from resources.Task import Tasks
+from resources.task import Tasks
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
